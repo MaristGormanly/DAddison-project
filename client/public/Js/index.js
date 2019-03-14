689888a39eb158f0a172515b2580d8385fd4b91d
Daniel Addison <Daniel.addison1@marist.edu>
2019-03-14 17:24:16 -0400

    Added Js to index.js file

--- client/public/Js/index.js
+++ client/public/Js/index.js
@@ -2,3 +2,16 @@ document.body.addEventListener("keyup", function(e){
   if (e.key == " ")
     window.location.href = "/mainmenu.html";
 });
+window.onload = function() {
+  fadeout();
+}
+
+function fadeout() {
+  document.getElementById('fadetext').style.opacity = '0';
+  window.setTimeout(fadein, 1000); //1 second
+}
+
+function fadein() {
+  document.getElementById('fadetext').style.opacity = '1';
+  window.setTimeout(fadeout, 1000); //1 second
+}
