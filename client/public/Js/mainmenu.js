c8ecce8ce9bb7249ed025dd41c711b0bc3b278e9
Daniel Addison <Daniel.addison1@marist.edu>
2019-03-14 17:45:22 -0400

    working on mainmenu.js/ adding turn on/off audio

---
+++ client/public/Js/mainmenu.js
@@ -0,0 +1,8 @@
+document.body.addEventListener("keyup", function(e){
+  if (e.key == "1")
+    window.location.href = "/trail.html";
+  if (e.key == "3")
+    window.location.href = "/topten.html";
+  //if (e.key == "4")
+    //window.location.href = "/topten.html";
+});
