73906c5124a8eaa765822ca6c084a36a568f18f9
Daniel Addison <Daniel.addison1@marist.edu>
2019-03-15 12:30:24 -0400

    added sound to mainmenu.js

--- client/public/Js/mainmenu.js
+++ client/public/Js/mainmenu.js
@@ -3,6 +3,20 @@ document.body.addEventListener("keyup", function(e){
     window.location.href = "/trail.html";
   if (e.key == "3")
     window.location.href = "/topten.html";
-  //if (e.key == "4")
-    //window.location.href = "/topten.html";
+  if (e.key == "4")
+    toggleaudio();
 });
+
+function toggleaudio() {
+  var audioplayer = document.getElementById("audioplayer");
+  var audiobutton = document.getElementById("audiobutton");
+
+  if (audioplayer.volume === 1){
+    audioplayer.volume = 0;
+    audiobutton.value = "Turn Sound (On)";
+  }
+  else {
+    audioplayer.volume = 1;
+    audiobutton.value = "Turn Sound (Off)";
+  }
+}
